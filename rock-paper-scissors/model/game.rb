d91e4f3ff7e330271c91c763a859e2550eeee29c
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-29 01:57:22 -0300

    poor implementation of Game#play

--- rock-paper-scissors/model/game.rb
+++ rock-paper-scissors/model/game.rb
@@ -4,13 +4,23 @@ class Game
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @stats = {
+      player_1 => 0,
+      player_2 => 0
+    }
   end
 
   def play!
+    @stats[player_1] += 1 if player_1.beats? player_2
+    @stats[player_2] += 1 if player_2.beats? player_1
   end
 
   def winner
-    player_2
+    if @stats[player_1] > @stats[player_2]
+      player_1
+    elsif @stats[player_2] > @stats[player_1]
+      player_2
+    end
   end
 
 end
--- rock-paper-scissors/spec/game_spec.rb
+++ rock-paper-scissors/spec/game_spec.rb
@@ -9,8 +9,16 @@ describe Game do
 
   it 'expect player 2 to win in round 3' do
     allow(player_1).to receive(:beats?).with(player_2).and_return(true, false, false)
+    allow(player_2).to receive(:beats?).with(player_1).and_return(false, true, true)
     3.times { subject.play! }
     expect(subject.winner).to eq(player_2)
+  end
+
+  it 'expect player 1 to win' do
+    allow(player_1).to receive(:beats?).with(player_2).and_return(true, true, false)
+    allow(player_2).to receive(:beats?).with(player_1).and_return(false, false, false)
+    3.times { subject.play! }
+    expect(subject.winner).to eq(player_1)
   end
   
 end
