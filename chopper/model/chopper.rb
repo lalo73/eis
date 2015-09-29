617b31dfce32615b1d7724c337dbea84df9b6bd5
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-28 23:17:20 -0300

    fix code format

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,41 +1,41 @@
 class Chopper
 
-    HUMANIZE_LIST = %w(cero uno dos tres cuatro cinco seis siete ocho nueve)
+  HUMANIZE_LIST = %w(cero uno dos tres cuatro cinco seis siete ocho nueve)
 
-    def chop(number, list)
-        index = list.index number
-        if index
-            index
-        else
-            -1
-        end
+  def chop(number, list)
+    index = list.index number
+    if index
+      index
+    else
+      -1
     end
+  end
 
-    def sum(list)
-        if list.size == 0
-            'vacio'
-        else
-            total = sum_all(list)
-            if total > 99
-                'demasiado grande'
-            else
-                humanized = humanize_all(total.to_s)
-                humanized.join(",")
-            end
-        end
+  def sum(list)
+    if list.size == 0
+      'vacio'
+    else
+      total = sum_all(list)
+      if total > 99
+        'demasiado grande'
+      else
+        humanized = humanize_all(total.to_s)
+        humanized.join(",")
+      end
     end
+  end
 
-    private
+  private
 
-    def humanize_all(list)
-        list.each_char.to_a.map { |number| humanize number.to_i }
-    end
+  def humanize_all(list)
+    list.each_char.to_a.map { |number| humanize number.to_i }
+  end
 
-    def sum_all(list)
-        list.inject(0) {|result, element| result + element }
-    end
+  def sum_all(list)
+    list.inject(0) {|result, element| result + element }
+  end
 
-    def humanize(number)
-        HUMANIZE_LIST[number]
-    end
+  def humanize(number)
+    HUMANIZE_LIST[number]
+  end
 end
