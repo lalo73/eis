63354f9506fc5d33345e457a8779c013cf69ed70
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-29 01:46:04 -0300

    player wins when beat 2 times at least

---
+++ rock-paper-scissors/model/game.rb
@@ -0,0 +1,16 @@
+class Game
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def play!
+  end
+
+  def winner
+    player_2
+  end
+
+end
---
+++ rock-paper-scissors/spec/game_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../model/game'
+
+describe Game do
+
+  let(:player_1) { double("Player") }
+  let(:player_2) { double("Player") }
+  subject { described_class.new player_1, player_2 }
+
+  before :each do
+    allow(player_1).to receive(:beats?).with(player_2).and_return(true, false, false)
+  end
+
+  it 'expect player 2 to win in round 3' do
+    3.times { subject.play! }
+    expect(subject.winner).to eq(player_2)
+
+  end
+end
