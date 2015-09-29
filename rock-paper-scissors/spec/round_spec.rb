c181da888b29c930041f8e5313934722cc5d0150
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-29 02:18:52 -0300

    refactor Paper players specs

--- rock-paper-scissors/spec/round_spec.rb
+++ rock-paper-scissors/spec/round_spec.rb
@@ -7,26 +7,19 @@ describe "Round" do
 
   let(:paper_player) { player(&:paper!) }
   let(:rock_player) { player(&:rock!) }
+  let(:scissors_player) { player(&:scissors!) }
+  let(:monkey_player) { player(&:monkey!) }
 
   it { expect(paper_player).to beat(rock_player) }
 
-  it 'expect paper to not beat scissors' do
-    player_1.paper!
-    player_2.scissors!
-    expect(player_1).to_not beat(player_2)
-  end
+  it { expect(paper_player).to_not beat(scissors_player) }
 
   it 'expect paper to not beat paper' do
-    player_1.paper!
     player_2.paper!
-    expect(player_1).to_not beat(player_2)
+    expect(paper_player).to_not beat(player_2)
   end
 
-  it 'expect paper to not beat Monkey' do
-    player_1.paper!
-    player_2.monkey!
-    expect(player_1).to_not beat(player_2)
-  end
+  it { expect(paper_player).to_not beat(monkey_player) }
 
   it 'expect rock beat scissors' do
     player_1.rock!
