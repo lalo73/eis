4afe51dd83a51efa8e5f973df0e7f464c1362ce9
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-29 00:37:38 -0300

    add spec 'rock does not beat rock'

--- rock-paper-scissors/spec/round_spec.rb
+++ rock-paper-scissors/spec/round_spec.rb
@@ -46,4 +46,10 @@ describe "Round" do
     player_2.paper!
     expect(player_1).to_not beat(player_2)
   end
+
+  it 'expect rock to not beat rock' do
+    player_1.rock!
+    player_2.rock!
+    expect(player_1).to_not beat(player_2)
+  end
 end
