4c6e4d17d4aa2dc51886688859f4d1f73a091f6e
Leandro Gomez <leandro.gz73@gmail.com>
2015-09-29 01:47:11 -0300

    minor refactor

--- rock-paper-scissors/spec/game_spec.rb
+++ rock-paper-scissors/spec/game_spec.rb
@@ -7,13 +7,10 @@ describe Game do
   let(:player_2) { double("Player") }
   subject { described_class.new player_1, player_2 }
 
-  before :each do
-    allow(player_1).to receive(:beats?).with(player_2).and_return(true, false, false)
-  end
-
   it 'expect player 2 to win in round 3' do
+    allow(player_1).to receive(:beats?).with(player_2).and_return(true, false, false)
     3.times { subject.play! }
     expect(subject.winner).to eq(player_2)
-
   end
+  
 end
