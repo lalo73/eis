608ef3c7ad534793bd7943b43232e3757f68ceb0
Leandro Gomez <leandro.gz73@gmail.com>
2015-10-12 20:41:59 -0300

    BattleshipGame knows if is empty

--- battleship/model/battleship_game.rb
+++ battleship/model/battleship_game.rb
@@ -1,7 +1,20 @@
 require_relative './ship'
+require_relative './board'
 
 class BattleshipGame
+
+  def initialize
+    @board = Board.new
+  end
   def build_destructor
     Ship.new
+  end
+
+  def place_horizontally(ship, x, y)
+    @board.place_horizontally(ship, x, y)
+  end
+
+  def empty?
+    @board.empty?
   end
 end
--- battleship/spec/battleship_game_spec.rb
+++ battleship/spec/battleship_game_spec.rb
@@ -4,7 +4,26 @@ require_relative '../model/ship'
 describe BattleshipGame do
   subject { described_class.new }
 
+  it { expect(subject).to be_empty }
+
   describe '#build_desctructor' do
     it { expect(subject.build_destructor).to be_an_instance_of(Ship) }
+  end
+
+  describe '#place' do
+    let(:ship) { Ship.new }
+    let(:positionA) { "A" }
+    let(:positionB) { "1" }
+
+    context 'with a empty board' do
+      before :each do
+        subject.place_horizontally ship, positionA, positionB
+      end
+
+      it {expect(subject).to_not be_empty}
+
+      #it {expect(subject.taken? positionA, positionB).to be_true}
+
+    end
   end
 end
