071287491cc8edb34ebb55f5b19f81ae20322ee2
Leandro Gomez <leandro.gz73@gmail.com>
2015-10-12 21:57:51 -0300

    refactor specs

--- battleship/spec/board_spec.rb
+++ battleship/spec/board_spec.rb
@@ -5,10 +5,59 @@ require_relative '../model/ship'
 describe Board do
   subject { described_class.new }
 
+  let(:positionA) { "A" }
+  let(:positionB) { "1" }
+
+  let(:vertical_ship) { double("Ship", horizontally?: false, vertically?: true, vertically?: true,vertically!: nil, lenght: 3) }
+  let(:horizontal_ship) { double("Ship", horizontally?: true,horizontally!: nil, lenght: 3) }
+
   it { expect(subject).to be_empty }
 
+  describe '#taken?' do
+    it {expect(subject).to_not be_taken(positionA, positionB)}
+
+    context 'after calling #place_horizontally' do
+      let(:ship) { horizontal_ship }
+      before :each do
+        subject.place_horizontally ship, positionA, positionB
+      end
+
+      it {expect(subject).to be_taken(positionA, positionB)}
+
+      it "takes until ship's lenght" do
+        delta_position = positionB.to_i + ship.lenght - 1
+        expect(subject).to be_taken(positionA, delta_position)
+      end
+
+      it "doesn't take more than ship's lenght" do
+        delta_position = positionB.to_i + ship.lenght
+        expect(subject).to_not be_taken(positionA, delta_position)
+      end
+    end
+
+    context 'after calling #place_vertically' do
+      let(:ship) { vertical_ship }
+      before :each do
+        subject.place_vertically ship, positionA, positionB
+      end
+
+      it {expect(subject).to be_taken(positionA, positionB)}
+
+      it "takes until ship's lenght" do
+        delta_position = (positionA.ord + ship.lenght - 1).chr
+        expect(subject).to be_taken(delta_position, positionB)
+      end
+
+      it "doesn't take more than ship's lenght" do
+        delta_position = (positionA.ord + ship.lenght).chr
+        expect(subject).to_not be_taken(delta_position, positionB)
+      end
+    end
+
+  end
+
   describe '#place_horizontally' do
-    let(:ship) { double("Ship", horizontally?: true,horizontally!: nil, lenght: 3) }
+    let(:ship) { horizontal_ship }
     let(:positionA) { "A" }
     let(:positionB) { "1" }
 
@@ -20,27 +69,11 @@ describe Board do
       it {expect(subject).to_not be_empty}
       it {expect(ship).to have_received(:horizontally!)}
 
-      describe "#taken?" do
-
-        it {expect(subject).to be_taken(positionA, positionB)}
-
-        it "takes until ship's lenght" do
-          delta_position = positionB.to_i + ship.lenght - 1
-          expect(subject).to be_taken(positionA, delta_position)
-        end
-
-        it "doesn't take more than ship's lenght" do
-          delta_position = positionB.to_i + ship.lenght
-          expect(subject).to_not be_taken(positionA, delta_position)
-        end
-      end
     end
   end
 
   describe '#place_vertically' do
-    let(:ship) { double("Ship", horizontally?: false, vertically?: true, vertically?: true,vertically!: nil, lenght: 3) }
-    let(:positionA) { "A" }
-    let(:positionB) { "1" }
+    let(:ship) { vertical_ship }
 
     context 'with a empty board' do
       before :each do
@@ -49,22 +82,6 @@ describe Board do
 
       it {expect(subject).to_not be_empty}
       it {expect(ship).to have_received(:vertically!)}
-
-      describe "#taken?" do
-
-        it {expect(subject).to be_taken(positionA, positionB)}
-
-        it "takes until ship's lenght" do
-          delta_position = (positionA.ord + ship.lenght - 1).chr
-          expect(subject).to be_taken(delta_position, positionB)
-        end
-
-        it "doesn't take more than ship's lenght" do
-          delta_position = (positionA.ord + ship.lenght).chr
-          expect(subject).to_not be_taken(delta_position, positionB)
-        end
-
-      end
     end
   end
 end
